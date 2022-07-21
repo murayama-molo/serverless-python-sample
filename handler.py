23022ac41457384caeea5a2af164bf26ba9c6549
murayama-molo <murayama@molo.co.jp>
2022-07-21 18:11:57 +0900

    serverless offlineでget postが確認できるように＆vscodeでデバッグできるように

--- handler.py
+++ handler.py
@@ -1,9 +1,10 @@
 import json
 
 
-def hello(event, context):
+def getHello(event, context):
+    print(event['query']["test"]) # get
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "[GET] Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
 
@@ -18,7 +19,31 @@ def hello(event, context):
     # integration
     """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "[GET] Go Serverless v1.0! Your function executed successfully!",
         "event": event
     }
     """
+
+def postHello(event, context):
+    print(event['body']["test"]) # post
+    body = {
+        "message": "[POST] Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "[POST] Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
+
